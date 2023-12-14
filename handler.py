5d391965e6c51d04677a48491282efddb971178d
Nimrod Saharof <nimrod@Nimrods-MacBook-Pro-2.local>
2023-12-14 21:22:38 +0200

    debug

--- handler.py
+++ handler.py
@@ -3,9 +3,9 @@ from base_post_api import make_post_request
 from base_get_api import make_get_request
 from download_video import download_video
 
-script_text = "Hi My name is Gal"
+script_text = "Hi My name is Nimi"
 source_url = "https://create-images-results.d-id.com/google-oauth2%7C111069582955618623865/upl_Lkv76YmBADLqUYVEr0hgO/image.jpeg"
-authorization = "Basic XXXs"
+authorization = "Basic xxx"
 
 id = make_post_request(script_text, source_url, authorization)
 
